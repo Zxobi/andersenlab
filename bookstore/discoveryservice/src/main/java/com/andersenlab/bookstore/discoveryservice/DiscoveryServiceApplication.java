4666c2826bb482c7f7ec7dd2d3a14248c8ca9dfe
Daniil Videneev <danyavid@gmail.com>
2020-03-05 16:55:05 +0300

    Add discovery service

---
+++ bookstore/discoveryservice/src/main/java/com/andersenlab/bookstore/discoveryservice/DiscoveryServiceApplication.java
@@ -0,0 +1,16 @@
+package com.andersenlab.bookstore.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+
+}
---
+++ bookstore/discoveryservice/src/test/java/com/andersenlab/bookstore/discoveryservice/DiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andersenlab.bookstore.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
