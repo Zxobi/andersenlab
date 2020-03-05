9f1135ccc55028a4a3be52076d23ee9f179dd28b
Daniil Videneev <danyavid@gmail.com>
2020-03-05 16:55:57 +0300

    Add eureka discovery service registration

--- bookstore/bookservice/src/main/java/com/andersenlab/bookstore/bookservice/BookServiceApplication.java
+++ bookstore/bookservice/src/main/java/com/andersenlab/bookstore/bookservice/BookServiceApplication.java
@@ -2,12 +2,14 @@ package com.andersenlab.bookstore.bookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableJpaRepositories
+@EnableDiscoveryClient
 public class BookServiceApplication {
 
     public static void main(String[] args) {
